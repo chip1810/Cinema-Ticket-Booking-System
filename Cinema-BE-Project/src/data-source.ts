207b995165878a372fcb8e74282d454cf8082edc
PhToan27 <phuctoan.qng2019@gmail.com>
2026-03-02 14:38:40 +0700

    fix:connect_db

--- Cinema-BE-Project/src/data-source.ts
+++ Cinema-BE-Project/src/data-source.ts
@@ -1,23 +1,16 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
-import { Genre } from "./entities/Genre";
-import { Movie } from "./entities/Movie";
-import { Hall } from "./entities/Hall";
-import { Showtime } from "./entities/Showtime";
-import { Concession } from "./entities/Concession";
-import { User } from "./entities/User";
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT || "3306"),
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "cinema_db",
-    synchronize: true,
-    logging: false,
-    entities: [Genre, Movie, Hall, Showtime, Concession, User],
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: true,
+  logging: true,
+  entities: [__dirname + "/entities/*.{ts,js}"],
 });
